e09c63c2a02b68aa516d8a4d3b7283364b2e6c74
andrey_vasiliev <mrshoffen@gmail.com>
2025-04-30 04:27:55 +0300

    fix: fix bug with encrypted parameter in key

--- src/main/java/org/mrshoffen/tasktracker/apigateway/security/SecurityConfiguration.java
+++ src/main/java/org/mrshoffen/tasktracker/apigateway/security/SecurityConfiguration.java
@@ -7,14 +7,18 @@ import org.mrshoffen.tasktracker.apigateway.security.service.JwtSignatureValidat
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 import java.text.ParseException;
+
+import static org.springframework.util.StringUtils.*;
 
 @Configuration
 public class SecurityConfiguration {
 
     @Bean
     JwtSignatureValidator jwtValidator(@Value("${jwt-user.keys.access-token-key}") String accessKey) throws ParseException, JOSEException {
-        return new JwtSignatureValidator(new MACVerifier(OctetSequenceKey.parse(accessKey)));
+        String key = trimLeadingCharacter(trimTrailingCharacter(accessKey, '\''), '\'');
+        return new JwtSignatureValidator(new MACVerifier(OctetSequenceKey.parse(key)));
     }
 }
