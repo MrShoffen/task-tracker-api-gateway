44afae965fbd330377fca95a1af56f27b753bb74
andrey_vasiliev <mrshoffen@gmail.com>
2025-05-30 22:38:24 +0300

    feat: move rate limiter to props

--- src/main/java/org/mrshoffen/tasktracker/apigateway/GatewayConfiguration.java
+++ src/main/java/org/mrshoffen/tasktracker/apigateway/GatewayConfiguration.java
@@ -12,25 +12,18 @@ import reactor.core.publisher.Mono;
 @Configuration
 public class GatewayConfiguration {
 
-    private static final Logger log = LoggerFactory.getLogger(GatewayConfiguration.class);
-
     @Bean
     public RedisRateLimiter redisRateLimiter(@Value("${app.gateway.rate-limiter.replenish}") Integer replenishRate,
                                              @Value("${app.gateway.rate-limiter.burst}") Integer burstCapacity,
                                              @Value("${app.gateway.rate-limiter.requested}") Integer requestedToken
     ) {
-        System.out.println("replenishRate: " + replenishRate);;
         return new RedisRateLimiter(replenishRate, burstCapacity, requestedToken);
     }
 
     @Bean
     KeyResolver ipKeyResolver() {
-        return exchange -> {
-            String hostAddress = exchange.getRequest().getRemoteAddress().getAddress().getHostAddress();
-            return Mono.just(
-                    exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
-
-        };
+        return exchange -> Mono.just(
+                exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
     }
 
 }
