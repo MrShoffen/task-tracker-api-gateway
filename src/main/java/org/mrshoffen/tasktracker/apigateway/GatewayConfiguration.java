cc5a25ad1fe8c6ad13f4d0207f4d87c80e575cdd
andrey_vasiliev <mrshoffen@gmail.com>
2025-05-30 21:53:27 +0300

    feat: move rate limiter to props

--- src/main/java/org/mrshoffen/tasktracker/apigateway/GatewayConfiguration.java
+++ src/main/java/org/mrshoffen/tasktracker/apigateway/GatewayConfiguration.java
@@ -1,5 +1,7 @@
 package org.mrshoffen.tasktracker.apigateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
@@ -10,11 +12,14 @@ import reactor.core.publisher.Mono;
 @Configuration
 public class GatewayConfiguration {
 
+    private static final Logger log = LoggerFactory.getLogger(GatewayConfiguration.class);
+
     @Bean
     public RedisRateLimiter redisRateLimiter(@Value("${app.gateway.rate-limiter.replenish}") Integer replenishRate,
                                              @Value("${app.gateway.rate-limiter.burst}") Integer burstCapacity,
                                              @Value("${app.gateway.rate-limiter.requested}") Integer requestedToken
     ) {
+        System.out.println("replenishRate: " + replenishRate);;
         return new RedisRateLimiter(replenishRate, burstCapacity, requestedToken);
     }
 
